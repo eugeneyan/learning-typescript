9c38975a0908cb9e36fa8e139482fb9198506a7d
eugeneyan <eugeneyanziyou@gmail.com>
2021-01-20 20:16:54 -0800

    Complete day 1

---
+++ basics-i/day1/main.js
@@ -0,0 +1,22 @@
+console.log('Hello World!');
+console.log(3 / 2);
+console.log(3 % 2);
+console.log(5 / 3);
+console.log(5 % 3);
+console.log(Math.pow(2, 4));
+var undefinedValue;
+console.log(undefinedValue);
+var emptyValue = null;
+console.log(emptyValue);
+var firstName = 'Eugene';
+var lastName = 'Yan';
+var country = 'US';
+var city = 'Seattle';
+var age = 34;
+var isMarried = true;
+console.log(firstName, lastName, country, city, age, isMarried);
+// This is a single line comment
+/* This is a
+   multi-line
+   comment
+ */ 
---
+++ basics-i/day1/main.ts
@@ -0,0 +1,32 @@
+console.log('Hello World!')
+
+console.log(3 / 2)
+
+console.log(3 % 2)
+
+console.log(5 / 3)
+console.log(5 % 3)
+
+console.log(2 ** 4)
+
+let undefinedValue
+console.log(undefinedValue)
+
+let emptyValue = null
+console.log(emptyValue)
+
+let firstName = 'Eugene'
+let lastName = 'Yan'
+let country = 'US'
+let city = 'Seattle'
+let age = 34
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age, isMarried)
+
+// This is a single line comment
+
+/* This is a
+   multi-line
+   comment
+ */
