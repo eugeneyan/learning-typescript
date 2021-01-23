583ff08909529a88783622298242bd1b540220ac
eugeneyan <eugeneyanziyou@gmail.com>
2021-01-22 18:50:34 -0800

    Completed day two

--- basics-i/day2/main.js
+++ basics-i/day2/main.js
@@ -23,3 +23,32 @@ console.log(Math.min(1, 3, 3, 4, 5));
 console.log(Math.max(1, 3, 3, 4, 5));
 var randNum = Math.random();
 console.log(randNum);
+var firstName = 'Eugene';
+var lastName = 'Yan';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log('Each \nword \nis \na \nnew \nline');
+console.log('shouldn\'t, couldn\'t, wouldn\'t');
+// String interpolation
+var a = 2;
+var b = 3;
+console.log("The sum of " + a + " and " + b + " is " + (a + b));
+// String methdods
+console.log(firstName.length);
+console.log(firstName[0]);
+console.log(firstName[firstName.length - 1]);
+console.log(firstName.substr(1, 3));
+console.log(firstName.substring(1, 3)); // Doesn't include char at end index
+// Splitting strings
+var sentence = 'This is a sentence';
+console.log(sentence.split(' '));
+console.log(sentence.split(' ')[0]);
+// Checking types
+console.log(typeof 'Eugene');
+console.log(typeof 8);
+console.log(typeof Math.PI);
+console.log(typeof true);
+// Casting
+var num = '10';
+var numInt = parseInt(num);
+console.log(num + " (" + typeof num + "), " + numInt + " (" + typeof numInt + ")");
--- basics-i/day2/main.ts
+++ basics-i/day2/main.ts
@@ -32,3 +32,41 @@ console.log(Math.max(1, 3, 3, 4, 5))
 
 let randNum = Math.random()
 console.log(randNum)
+
+let firstName = 'Eugene'
+let lastName = 'Yan'
+
+let fullName = firstName + ' ' + lastName
+console.log(fullName)
+
+console.log('Each \nword \nis \na \nnew \nline')
+console.log('shouldn\'t, couldn\'t, wouldn\'t')
+
+// String interpolation
+let a = 2
+let b = 3
+console.log(`The sum of ${a} and ${b} is ${a + b}`)
+
+// String methdods
+console.log(firstName.length)
+console.log(firstName[0])
+console.log(firstName[firstName.length - 1])
+
+console.log(firstName.substr(1, 3))
+console.log(firstName.substring(1, 3))  // Doesn't include char at end index
+
+// Splitting strings
+let sentence = 'This is a sentence'
+console.log(sentence.split(' '))
+console.log(sentence.split(' ')[0])
+
+// Checking types
+console.log(typeof 'Eugene')
+console.log(typeof 8)
+console.log(typeof Math.PI)
+console.log(typeof true)
+
+// Casting
+let num: string = '10'
+let numInt: number = parseInt(num)
+console.log(`${num} (${typeof num}), ${numInt} (${typeof numInt})`)
