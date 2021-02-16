fc6f287685728017cb01b6e6bfd1288df2f3873f
eugeneyan <eugeneyanziyou@gmail.com>
2021-02-15 20:26:28 -0800

    Complete day 14

---
+++ basics-i/day14/main.ts
@@ -0,0 +1,52 @@
+export { }
+/* 
+ * try: wrap code that may throw an error in a try block
+ * catch: write code to do something when an error occurs
+ * - The catch block takes a parameter, and it's common to pass e, err, or error as parameter
+ * finally: write code to always be executed regardless of error
+ */
+
+try {
+    let lastName = 'Yan'
+    let fullName = `${firstName} ${lastName}`
+    console.log(fullName)
+} catch (e) {
+    console.log(`Name of error: ${e.name}`)
+    console.log(`Error message: ${e.message}`)
+} finally {
+    console.log('This log will also be printed')
+}
+
+// Throw: allows us to create customer error
+// throw 'Custom error'
+// throw 42
+// throw true
+// throw new Error('This is a custom error')
+
+// Note: prompt() is not a valid construct in Node.js runtime, but it'll work in browser console
+// const throwErrorFunc = () => {
+//     let message
+//     let x = window.prompt('Enter a number: ')
+//     try {
+//         if (x == '') throw 'Empty input'
+//         if (isNaN(x)) throw 'Not a number'
+//         let numx = Number(x)
+//         if (numx < 5) throw 'Number is too low'
+//         if (numx > 10) throw 'Number is too high'
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+// throwErrorFunc()
+
+// Types of error
+let lastName = 'Yan'
+// let fullName = `${firstName} ${lastName}`
+// console.log(fullName)  // ReferenceError as we try to use firstName that has not been declared
+
+let square = 2 x 2  // SyntaxError in JavaScript, but okay in TypeScript
+console.log(square)
+
+// Type error
+let num = 10
+console.log(num.toLowerCase())  // TypeError
