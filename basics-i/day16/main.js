4bbb26238c31fa516bacfd834d321830fb124954
eugeneyan <eugeneyanziyou@gmail.com>
2021-02-22 19:26:07 -0800

    Complete day 16

---
+++ basics-i/day16/main.js
@@ -0,0 +1,104 @@
+/*
+ * JSON stands for JavaScript Object Notation. The format is text or string only.
+ * It is a lightweight data format for storing and transporting, and is mostly used when
+ * data is sent from a server to a client. It's easier than XML.
+ * JavaScript Object and JSON are very similar and interchangable.
+ *
+ * - The key of a JSON object should be double quotes and it should be string.
+ */
+// JSON.parse() to change JSON into object; JSON.stringfy() to change object to JSON.
+var usersText = "{\n    \"users\":[\n      {\n        \"firstName\":\"Asabeneh\",\n        \"lastName\":\"Yetayeh\",\n        \"age\":250,\n        \"email\":\"asab@asb.com\"\n      },\n      {\n        \"firstName\":\"Alex\",\n        \"lastName\":\"James\",\n        \"age\":25,\n        \"email\":\"alex@alex.com\"\n      },\n      {\n      \"firstName\":\"Lidiya\",\n      \"lastName\":\"Tekle\",\n      \"age\":28,\n      \"email\":\"lidiya@lidiya.com\"\n      }\n    ]\n    }";
+var userObject = JSON.parse(usersText);
+console.log(userObject);
+// Using a reviver function as a formatter (e.g., formatting first name and last name)
+// ? is a ternary operator: condition ? execute if truthy : execute if falsy
+var userObject2 = JSON.parse(usersText, function (key, value) {
+    var newValue = typeof value == 'string' && key != 'email' ? value.toUpperCase() : value;
+    return newValue;
+});
+console.log(userObject2);
+// Converting object to JSON
+var users = {
+    Alex: {
+        email: 'alex@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript'],
+        age: 20,
+        isLoggedIn: false,
+        points: 30
+    },
+    Asab: {
+        email: 'asab@asab.com',
+        skills: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'Redux',
+            'MongoDB',
+            'Express',
+            'React',
+            'Node'
+        ],
+        age: 25,
+        isLoggedIn: false,
+        points: 50
+    },
+    Brook: {
+        email: 'daniel@daniel.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+        age: 30,
+        isLoggedIn: true,
+        points: 50
+    },
+    Daniel: {
+        email: 'daniel@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    John: {
+        email: 'john@john.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+        age: 20,
+        isLoggedIn: true,
+        points: 50
+    },
+    Thomas: {
+        email: 'thomas@thomas.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    Paul: {
+        email: 'paul@paul.com',
+        skills: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'MongoDB',
+            'Express',
+            'React',
+            'Node'
+        ],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    }
+};
+var usersText2 = JSON.stringify(users);
+console.log(usersText2);
+// Using a filter array to filter the keys that we want to keep
+var user = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    country: 'Finland',
+    city: 'Helsinki',
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Pyhton'],
+    age: 250,
+    isLoggedIn: false,
+    points: 30
+};
+var userText = JSON.stringify(user, ['firstName', 'lastName']);
+console.log(userText);
